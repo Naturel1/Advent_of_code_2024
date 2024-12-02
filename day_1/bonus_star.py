0a9f066647b5c3e02d944c0829f01cadf822dec5
Naturel1 <moi@arnaudhubert.be>
2024-12-02 16:45:52 +0100

    day 1

---
+++ day_1/bonus_star.py
@@ -0,0 +1,22 @@
+def import_problem_input(path: str) -> list[list]:
+    with open(path, 'r') as file:
+        listes = [[], []]
+        for line in file:
+            split_line = line.split()
+            listes[0].append(int(split_line[0]))
+            listes[1].append(int(split_line[1]))
+    return listes
+
+def process_lists(listes: list) -> list:
+    result = []
+    for i in range(len(listes[0])):
+        result.append(listes[0][i] * listes[1].count(listes[0][i]))
+    return result
+
+def main():
+    listes = import_problem_input('input_1.txt')
+    processed_lists = process_lists(listes)
+    print(sum(processed_lists))
+
+if __name__ == "__main__":
+    main()
---
+++ day_1/first_star.py
@@ -0,0 +1,27 @@
+def import_problem_input(path: str) -> list[list]:
+    with open(path, 'r') as file:
+        listes = [[], []]
+        for line in file:
+            split_line = line.split()
+            listes[0].append(int(split_line[0]))
+            listes[1].append(int(split_line[1]))
+    return listes
+
+def sort_lists(listes: list) -> list:
+    liste_1 = sorted(listes[0])
+    liste_2 = sorted(listes[1])
+    return [liste_1, liste_2]
+
+def calculate_distance(listes: list) -> list[int]:
+    distances = []
+    for a, b in zip(listes[0], listes[1]):
+        distances.append(abs(a - b))
+    return distances
+
+def main():
+    listes = import_problem_input('input_1.txt')
+    distances = calculate_distance(sort_lists(listes))
+    print(sum(distances))
+
+if __name__ == "__main__":
+    main()
